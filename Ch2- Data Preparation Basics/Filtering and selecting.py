6c205c33254486a9ddf909f1c552c7560d81f8df
DESKTOP-GVP3BTH\USER <gkyanuzov@gmail.com>
2023-06-16 22:08:07 +0300

    added extra libraries

---
+++ Ch2- Data Preparation Basics/Filtering and selecting.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+from pandas import Series,DataFrame
+
+series_obj = Series(np.arange(8), index=['row 1', 'row 2',
+                            'row 3','row 4','row 5',
+                            'row 6','row 7', 'row 8'])
+
+print(series_obj)
