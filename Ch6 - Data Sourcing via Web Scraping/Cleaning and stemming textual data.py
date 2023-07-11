de6808c6accbab20629460e8275649e118b29022
DESKTOP-GVP3BTH\USER <gkyanuzov@gmail.com>
2023-07-11 17:18:18 +0300

    finished exercise

---
+++ Ch6 - Data Sourcing via Web Scraping/Cleaning and stemming textual data.py
@@ -0,0 +1,34 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+text = "On Wednesday, the Association for Computing Machinery, the world’s largest society of computing " \
+       "professionals, announced that Hinton, LeCun and Bengio had won this year’s Turing Award for their work on " \
+       "neural networks. The Turing Award, which was introduced in 1966, is often called the Nobel Prize of " \
+       "computing, and it includes a $1 million prize, which the three scientists will share."
+word_tk = word_tokenize(text)
+
+# Removing stop words
+# nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+
+sw = set(stopwords.words('english'))
+print(f"Stop words in english are: {sw}")
+
+filtered_words = [w for w in word_tk if not w in sw]
+print(f"The text after removing stop words: {filtered_words}")
+
+# Stemming
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+port_stem = PorterStemmer()
+stemmed_words = []
+
+for w in filtered_words:
+    stemmed_words.append(port_stem.stem(w))
+
+print(f'Filtered Sentence: {filtered_words}')
+print(f'Stemmed Sentence: {stemmed_words}')
+
+
